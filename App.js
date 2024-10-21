1cee5438de408acc9f5b4b9409dd76c098b9e3a5
Dhaval <joshidhaval2002@gmail.com>
2024-10-21 14:42:26 +0530

    use array to add siblings

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 
 /**
  * <div id=parent>
- *  <div id=child>
- *      <h1> This is h1 </h1>
- *      </div>
+ *   <div id=child>
+ *      <h1 id='heading1'> This is h1 </h1>
+ *      <h2 id='heading2'> This is heading 2</h2>
+ *   </div>
  * </div>
  */
 
-// const heading=React.createElement("h1",{id:'headingid'},"Hello World from React")   //create Element part of core module,therefore used React module
-        
+
 //to create nested html elements
-const parent=React.createElement("div",{id:'parent'},React.createElement('div',{id:'child'},React.createElement('h1',{id:'heading1'},"This is heading 1")))
+const parent=React.createElement("div",{id:'parent'},React.createElement('div',{id:'child'},
+    [React.createElement('h1',{id:'heading1'},"This is heading 1"),React.createElement('h2',{id:'heading2'},'This is heading2')]))
 
 const root=ReactDOM.createRoot(document.getElementById('root'))     //createRoot is part of ReactDOM module
 
