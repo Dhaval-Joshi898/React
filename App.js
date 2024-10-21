aac6b1f3b337c954faad760050f169d1624180db
Dhaval <joshidhaval2002@gmail.com>
2024-10-21 14:51:10 +0530

    added 2 child and thier nested inside parents div

--- App.js
+++ App.js
@@ -5,14 +5,22 @@
  *      <h1 id='heading1'> This is h1 </h1>   //used array to add these siblings
  *      <h2 id='heading2'> This is heading 2</h2>  //used array to add these siblings
  *   </div>
+
+ *   <div id=child2>
+ *      <h1 id='heading1'> This is heading 1 of child 2 </h1>   //used array to add these siblings
+ *      <h2 id='heading2'> This is heading 2 of child 2</h2>  //used array to add these siblings
+ *   </div>
+ * 
  * </div>
  */
 
 
 //to create nested html elements
-const parent=React.createElement("div",{id:'parent'},React.createElement('div',{id:'child'},
-    [React.createElement('h1',{id:'heading1'},"This is heading 1"),React.createElement('h2',{id:'heading2'},'This is heading2')]))
+const parent = React.createElement("div", { id: 'parent' }, [React.createElement('div', { id: 'child' },
+    [React.createElement('h1', { id: 'heading1' }, "This is heading 1"), React.createElement('h2', { id: 'heading2' }, 'This is heading 2')]),
+React.createElement('div', { id: 'child2' },
+    [React.createElement('h1', { id: 'heading1' }, "This is heading 1 of child 2"), React.createElement('h2', { id: 'heading2' }, 'This is heading 2 of child 2')])])
 
-const root=ReactDOM.createRoot(document.getElementById('root'))     //createRoot is part of ReactDOM module
+const root = ReactDOM.createRoot(document.getElementById('root'))     //createRoot is part of ReactDOM module
 
 root.render(parent)
