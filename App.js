6b8b02bfc6c283e8eb949ab7db5baf93b3384ef7
Dhaval <joshidhaval2002@gmail.com>
2024-10-27 20:39:06 +0530

    mutli line code in JSX using  a parenthesis

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
 //Using core React
 // React ELement is OBJEct => to HTMl on (render)
 // const heading=React.createElement('h1',{id:"headings"},"Hello world from Core react")
@@ -11,34 +10,12 @@ import ReactDOM from 'react-dom/client';
 //USING JSX JSX :HTMl like syntax
 // JSX converted to react Element and then on render it converts to html for browser to understand
 const jsxheading=<h1 id='headingid' className='headingclass'>Hello World in react using JSX</h1>   
+
+const jsxMheading=(
+    <div id='mutli'>
+        <h1>Heading using JSX multi line</h1>
+    </div>
+)
+
 const root =ReactDOM.createRoot(document.getElementById('root'))
-root.render(jsxheading)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const parent = React.createElement("div", { id: 'parent' }, [React.createElement('div', { id: 'child' },
-//     [React.createElement('h1', { id: 'heading1' }, "This is heading 1"), React.createElement('h2', { id: 'heading2' }, 'This is heading 2')]),
-//     React.createElement('div', { id: 'child2' },
-//         [React.createElement('h1', { id: 'heading1' }, "This is heading 1 of child 2"), React.createElement('h2', { id: 'heading2' }, 'This is heading 2 of child 2')])])
-        
-// const root = ReactDOM.createRoot(document.getElementById('root'))     //createRoot is part of ReactDOM module
-        
-// root.render(parent) 
-
-
-
+root.render(jsxMheading)
