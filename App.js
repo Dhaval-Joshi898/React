4a2a0e7b521bbca4c1a6b8f6fb4d245dcc358a46
Dhaval <joshidhaval2002@gmail.com>
2024-10-28 20:54:40 +0530

    use retrun kyword to return jsx code when usig {} this brackets AND if not using this {} no need of retrun keyword it return automatically

--- App.js
+++ App.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-//Using core React
-// React ELement is OBJEct => to HTMl on (render)
-// const heading=React.createElement('h1',{id:"headings"},"Hello world from Core react")
-// const root =ReactDOM.createRoot(document.getElementById('root'))
-// root.render(heading)
-
-//USING JSX JSX :HTMl like syntax
-// JSX converted to react Element and then on render it converts to html for browser to understand
 
 //React Components: Class based components AND Functional Components
 
 //  Functional Components :This is a JAVASCRIPT FUNCTION which returns a JScode or React Element
 
-const compo=()=>{
-    const compoHeading=(
-        <h1 style={"color:pink"} >THis is a heading written inside component </h1>
-    )
-}
-const jsxheading=<h1 id='headingid' className='headingclass'>Hello World in react using JSX</h1>   
+//JS arrow function single line ---> 
 
-const jsxMheading=(
-    <div id='mutli'>
-        <h1>Heading using JSX multi line</h1>
+//const fn=()=> console.log('Heloo)  PEFECTLY valid
+const HeadCompo = () => <h1>Heading inside Heading component</h1>  //Component without return keyword
+//for multiline JSX code use () like below
+
+const BodyCompo = () =>
+(
+    <div id='container'>
+        <HeadCompo />
+        <h2>Body Component </h2>
+        <a href="www.google.com">Google</a>
     </div>
-) 
+)
 
-const root =ReactDOM.createRoot(document.getElementById('root'))
-root.render(jsxMheading) 
+//NOTE IF YOU are using CURLY Brackets {} then use  the "RETURN" keyword
+
+// const HeadCompo2 = () => {
+//   return  <h1>Heading inside Heading component 2</h1>
+// }
+
+// const BodyCompo2 = () =>{
+// return(
+//     <div id='container'>
+//         <HeadCompo2 />
+//         <h2>Body Component 2 </h2>
+//         <a href="www.google.com">Google</a>
+//     </div>
+// )
+// }
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<BodyCompo2 />)
