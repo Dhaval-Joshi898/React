12e5ba3f6e7958b52a6a2b3fa0f1c20c91381ff5
Dhaval <joshidhaval2002@gmail.com>
2024-10-28 20:30:41 +0530

    Component in reacts ,Functional component example

--- App.js
+++ App.js
@@ -9,13 +9,23 @@ import ReactDOM from 'react-dom/client';
 
 //USING JSX JSX :HTMl like syntax
 // JSX converted to react Element and then on render it converts to html for browser to understand
+
+//React Components: Class based components AND Functional Components
+
+//  Functional Components :This is a JAVASCRIPT FUNCTION which returns a JScode or React Element
+
+const compo=()=>{
+    const compoHeading=(
+        <h1 style={"color:pink"} >THis is a heading written inside component </h1>
+    )
+}
 const jsxheading=<h1 id='headingid' className='headingclass'>Hello World in react using JSX</h1>   
 
 const jsxMheading=(
     <div id='mutli'>
         <h1>Heading using JSX multi line</h1>
     </div>
-)
+) 
 
 const root =ReactDOM.createRoot(document.getElementById('root'))
-root.render(jsxMheading)
+root.render(jsxMheading) 
