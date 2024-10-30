182dfed0ce930134c1b7d50e8339e911b0afcd63
Dhaval <joshidhaval2002@gmail.com>
2024-10-30 19:25:38 +0530

    export link varaible

---
+++ utils/constants.js
@@ -0,0 +1,8 @@
+const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660,e_grayscale/"
+
+const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWycK1c1Xf9Hr8MrAwj16q1XlDyYy1euwybQ&s"
+
+//NOTE:There can be only 1 default export in each file .
+
+
+//For multiple export use export then name of variable and at time import use import {name of variable}
