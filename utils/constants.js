af4bbc9d2e1cbad47d9c61226009bcde45966f5f
Dhaval <joshidhaval2002@gmail.com>
2024-10-30 19:31:02 +0530

    for links i have created utlis and exporting it to use in COMPONENTS

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
-const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660,e_grayscale/"
+export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660,e_grayscale/"
 
-const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWycK1c1Xf9Hr8MrAwj16q1XlDyYy1euwybQ&s"
+export const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWycK1c1Xf9Hr8MrAwj16q1XlDyYy1euwybQ&s"
 
 //NOTE:There can be only 1 default export in each file .
 
