67e6ff6cb92aab9ea21332f1807ed8663734c27b
Dhaval <joshidhaval2002@gmail.com>
2024-10-30 12:17:23 +0530

    created component folder inside src

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+    return (
+        <div className='header'>
+
+            <div id="header-logo-container">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWycK1c1Xf9Hr8MrAwj16q1XlDyYy1euwybQ&s" alt="app-logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
