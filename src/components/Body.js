84cfa49629eb29623bd367fc0a5e008c810619b1
Dhaval <joshidhaval2002@gmail.com>
2024-11-05 15:17:06 +0530

    useState hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+
+    // Super local State varaibles
+    let [listOfRestaurants, setlist0fRestaurants] = useState(resList) //hooks
+
     return (
         <div className="body">
-            <div className='search'>
-                Search
+            <div className='filter'>
+                <button className="filter-btn"
+                    onClick={() => {
+
+                        let filtered = listOfRestaurants.filter((res) => res.data.avgRating > 4.1);
+                        console.log(filtered);
+                        setlist0fRestaurants(filtered)
+                    }}>Filter</button>
             </div>
             <div className='restaurant-container'>
 
-                {resList.map((restaurant) => {
-                    return < RestaurantCard key={restaurant.data.id} resData={restaurant} /> 
+                {listOfRestaurants.map((restaurant) => {
+                    return < RestaurantCard key={restaurant.data.id} resData={restaurant} />
                     //key is IMPORTANT to have unique key
                 })}
 
