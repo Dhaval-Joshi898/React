970c090f6b6716cda8354a4d952f538bb0308e4e
Dhaval <joshidhaval2002@gmail.com>
2024-11-05 19:05:42 +0530

    use of useEffect()

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const AppLayout = () => {
     return (
         <div className='app-container'>
             <Header />
+       
             <Body />
         </div>
     )
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
 
     // Super local State varaibles
     let [listOfRestaurants, setlist0fRestaurants] = useState(resList) //hooks
 
+    useEffect(()=>{
+        console.log('after ui rendered')
+    },[])
+
+    //fetching data from swiggy api
+    const fetchData
+
     return (
         <div className="body">
             <div className='filter'>
                 <button className="filter-btn"
                     onClick={() => {
-
                         let filtered = listOfRestaurants.filter((res) => res.data.avgRating > 4.1);
                         console.log(filtered);
                         setlist0fRestaurants(filtered)
                     }}>Filter</button>
             </div>
+
             <div className='restaurant-container'>
 
                 {listOfRestaurants.map((restaurant) => {
