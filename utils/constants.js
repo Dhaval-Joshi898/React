5cb68f5822340a203bb0d5066000fc5ab59d3a44
Dhaval <joshidhaval2002@gmail.com>
2024-11-07 20:02:44 +0530

    url changed

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660,e_grayscale/"
+export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660,/"
 
 export const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWycK1c1Xf9Hr8MrAwj16q1XlDyYy1euwybQ&s"
 
