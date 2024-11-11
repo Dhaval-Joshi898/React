de1481ce26ba1c80df498f9c1feea7642537bef9
Dhaval <joshidhaval2002@gmail.com>
2024-11-11 16:37:12 +0530

    About us component for routing

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+const About=()=>{
+    return( 
+        <div className="about-section">
+             <h1>About us Page</h1>
+             <h3>About us page thorugh router</h3>
+        </div>
+   
+    
+    )
+}
+
+export default About;
