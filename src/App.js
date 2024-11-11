0f09a41d5b36dc8c1b28312e84840d9c71cc0a3f
Dhaval <joshidhaval2002@gmail.com>
2024-11-11 23:31:06 +0530

    single page application compnent replace and children routing

--- src/App.js
+++ src/App.js
@@ -5,33 +5,39 @@ import Body from './components/Body';
 import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
-import { createBrowserRouter ,RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider,Outlet } from 'react-router-dom';
 
 const AppLayout = () => {
     return (
         <div className='app-container'>
             <Header />
-            <Body />
+            {/* <Body /> */}
+            <Outlet/>
         </div>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const appRouter=createBrowserRouter([
+const appRouter = createBrowserRouter([
     {
-        path:"/",
-        element:<AppLayout/>,
-        errorElement:<Error/>
+        path: "/",
+        element: <AppLayout />,
+        children: [{
+            path: "/",
+            element: <Body />,
+        },
+        {
+            path: "/about",
+            element: <About />,
+        },
+        {
+            path: "/contact",
+            element: <Contact />,
+        }],
+        errorElement: <Error />
     },
-    {
-        path:"/about",
-        element:<About />,
-    },
-    {
-        path:"/contact",
-        element:<Contact />,
-    }
+
 ]
 )
 
