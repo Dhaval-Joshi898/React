78380c63cca529ad31bb04ee4d2292e0d6f3bec9
Dhaval <joshidhaval2002@gmail.com>
2024-11-11 23:31:26 +0530

    isntwad anchor used Link router from react router dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,51 @@
 import { useEffect, useState } from "react";
 import { LOGO_URL } from "../../utils/constants";
 
+import { Link } from "react-router-dom";
+
 const Header = () => {
-    console.log('header render')
-    let [changeBtn,setBtn]=useState('Login')
+  console.log("header render");
+  let [changeBtn, setBtn] = useState("Login");
 
-    useEffect(()=>{
-        console.log('use effect header')
-    },[changeBtn])
-   
-    return (
-        <div className='header'>
+  useEffect(() => {
+    console.log("use effect header");
+  }, [changeBtn]);
 
-            <div id="header-logo-container">
-                <img src={LOGO_URL} alt="app-logo" />
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li >About us</li>
-                    <li>Contact us</li>
-                    <li>Cart</li>
-                    <button className="log-btn" onClick={()=>{
-                        if (changeBtn==="Login"){
-                            setBtn('Logout');
-                            // console.log(changeBtn)
-                        }
-                        else{
-                            setBtn("Login")
-                            // console.log(changeBtn)
-                        }
-                    }}>{changeBtn} </button>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="header">
+
+      <div id="header-logo-container">
+        <img src={LOGO_URL} alt="app-logo" />
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home </Link>
+          </li>
+          <li>
+            <a href="/about">About us</a>
+          </li>
+          <li>
+            <Link to="/contact">Contact us</Link>
+          </li>
+          <li>Cart</li>
+
+          <button
+            className="log-btn"
+            onClick={() => {
+              if (changeBtn === "Login") {
+                setBtn("Logout");
+                // console.log(changeBtn)
+              } else {
+                setBtn("Login");
+                // console.log(changeBtn)
+              }
+            }}  >{changeBtn} </button>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
