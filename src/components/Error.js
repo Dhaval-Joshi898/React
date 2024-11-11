7402c787b49cacf9a1d87494b85ad0fa13861434
Dhaval <joshidhaval2002@gmail.com>
2024-11-11 16:39:35 +0530

    error component created

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import { useRouteError } from "react-router-dom";
+
+const Error=()=>{
+   
+    return(
+      <div className="contact-section">
+        <h1>error</h1>
+        {/* <h2> {err.status}: {err.error.message}</h2> */}
+      </div>
+    )
+}
+export default Error;
