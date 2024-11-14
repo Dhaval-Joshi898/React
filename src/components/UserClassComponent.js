bade3de4fcfd470b43beea067a8ad86a2131d895
Dhaval <joshidhaval2002@gmail.com>
2024-11-14 14:56:17 +0530

    Classs based component created of User section

---
+++ src/components/UserClassComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div className="userInfo-container">
+        <h2>Dhaval Joshi</h2>
+        <h3>Developing CLASS based components</h3>
+        <h4>joshidhaval2002@gmail.com</h4>
+      </div>
+    );
+  }
+}
+
+
+export default UserClass;
