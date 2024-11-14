991b8e2a4448d6f31267209f5bce35771ed61614
Dhaval <joshidhaval2002@gmail.com>
2024-11-14 14:56:56 +0530

    added Class based componebnt and  userbased componet inside About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,19 @@
+import User from "./User";
+import UserClass from "./UserClassComponent";
 const About=()=>{
+    const obj={
+        name:"Dhaval",
+        mail:"joshidhaval2002@gmail.com"
+    }
+
     return( 
         <div className="about-section">
              <h1>About us Page</h1>
              <h3>About us page thorugh router</h3>
+
+             <User  data={obj}/>
+             <UserClass/>
         </div>
-   
-    
     )
 }
 
