b398708c653b21da1bd6e552536125a6e985b956
Dhaval <joshidhaval2002@gmail.com>
2024-11-14 20:21:56 +0530

    Class based component and how to pass and acess Props

--- src/components/UserClassComponent.js
+++ src/components/UserClassComponent.js
@@ -1,15 +1,22 @@
 import React from "react";
+
 class UserClass extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
   render() {
+    //destructuring props of class based components 
+    const {name,location}=this.props
+
     return (
       <div className="userInfo-container">
-        <h2>Dhaval Joshi</h2>
-        <h3>Developing CLASS based components</h3>
+        <h2>Name:{name}</h2>
+        <h3>location:{location}</h3>
         <h4>joshidhaval2002@gmail.com</h4>
       </div>
     );
   }
 }
-
 
 export default UserClass;
