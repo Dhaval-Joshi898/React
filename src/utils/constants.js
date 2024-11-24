bebe6a2200902af42805b86946616acfd3410452
Dhaval <joshidhaval2002@gmail.com>
2024-11-24 15:14:17 +0530

    utils folder inside src

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660,/"
+
+export const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWycK1c1Xf9Hr8MrAwj16q1XlDyYy1euwybQ&s"
+
+export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.2993420046926&lng=72.86991979871428&restaurantId="
+
+//NOTE:There can be only 1 default export in each file .
+
+
+//For multiple export use export then name of variable and at time import use import {name of variable}
