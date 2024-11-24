cd097583af37d54f8844bfb0cb4971c67e33e18d
Dhaval <joshidhaval2002@gmail.com>
2024-11-24 15:13:16 +0530

    added custom hook useRestaurantMenu for fetching the data and making this component responsible to only displaying the data

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,24 +1,11 @@
-import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
-
   const { resId } = useParams(); // Destructuring resId from dynamic route
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API + resId);
-    const json = await data.json();
-    // console.log(resId);
-    setResInfo(json.data);
-    // console.log(json.data);
-  };
+  const resInfo=useRestaurantMenu(resId)
 
   if (resInfo === null) {
     return <Shimmer />;
@@ -49,9 +36,7 @@ const RestaurantMenu = () => {
         <ul>
           {itemCards.map((item) => {
             return (
-              <li key={item?.card?.info?.id}>
-                {item?.card?.info?.name} - Rs{" "}
-                {item?.card?.info?.price / 100 || item?.card?.info?.defaultPrice / 100}
+              <li key={item?.card?.info?.id}> {item?.card?.info?.name} - Rs{" "}  {item?.card?.info?.price / 100 || item?.card?.info?.defaultPrice / 100}
               </li>
             );
           })}
