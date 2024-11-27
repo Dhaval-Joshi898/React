f39cd025507e45e30e86f34b9581c277fbbe2396
Dhaval <joshidhaval2002@gmail.com>
2024-11-27 23:15:31 +0530

    added restaurant categroy component inside restaurant menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,11 +1,12 @@
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
   const { resId } = useParams(); // Destructuring resId from dynamic route
 
-  const resInfo=useRestaurantMenu(resId)
+  const resInfo = useRestaurantMenu(resId)
 
   if (resInfo === null) {
     return <Shimmer />;
@@ -21,28 +22,31 @@ const RestaurantMenu = () => {
     (card) => card?.card?.card?.title === "Recommended"
     //the first card is name to current element of array i.e json so it is like card{card:{card:{title}}}
   );
-  
-  console.log("recommendedCArdssssssssssss",recommendedCard)
+
+  // console.log("recommendedCArdssssssssssss",recommendedCard)
   // Extract itemCards from the "Recommended" card if it exists
-  const {itemCards} = recommendedCard?.card?.card || [];
-  console.log("items cardsssssssss",itemCards)
+  const { itemCards } = recommendedCard?.card?.card || [];
+  // console.log("items cardsssssssss",itemCards)
+
+  // console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards)
+
+  const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c) => {
+    return c.card?.card?.["@type"] === 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory'
+  })
+
+  // console.log(categories)
 
   return (
-    <div className="menu-card-container">
-      <h2>{name}</h2>
-      <h3>{cuisines?.join(",")}</h3>
-      <h4>{costForTwoMessage}</h4>
-      <div className="menu-list">
-        <ul>
-          {itemCards.map((item) => {
-            return (
-              <li key={item?.card?.info?.id}> {item?.card?.info?.name} - Rs{" "}  {item?.card?.info?.price / 100 || item?.card?.info?.defaultPrice / 100}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+    <div className="text-center">
+      <h2 className="font-bold text-2xl my-4">{name}</h2>
+      <span className="font-bold text-lg"> {cuisines?.join(",")} - {costForTwoMessage}</span>
+
+      {categories.map((category) => {
+        return <RestaurantCategory data={category.card.card}></RestaurantCategory>
+      })}
+
     </div>
+
   );
 };
 
