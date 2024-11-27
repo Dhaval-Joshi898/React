cfd139a6ee83f2f9183c9634b812370eb927106f
Dhaval <joshidhaval2002@gmail.com>
2024-11-27 18:07:30 +0530

    designed using tailwind css

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,20 +1,20 @@
 import { CDN_URL } from "../utils/constants";
 
 const RestaurantCard = (prop) => {
-    
+
     // console.log(prop)    RETURNS OBJECT
     const { resData } = prop
-    
+
     // const {name,cuisines,costForTwo,deliveryTime,cloudinaryImageId,avgRating}=resData?.data
-    const {name,cuisines,costForTwo,sla,cloudinaryImageId,avgRating}=resData?.info
-    const {slaString}=sla
-   
+    const { name, cuisines, costForTwo, sla, cloudinaryImageId, avgRating } = resData?.info
+    const { slaString } = sla
+
     return (
-        <div className='restro-card' style={{ backgroundColor: 'f0f0f0' }}>
-            <div className='restro-logo'>
-                <img src={CDN_URL+cloudinaryImageId} alt="restro-logo" />
-            </div>
-            <h3>{name}</h3>
+        <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200">
+
+            <img className="rounded-lg" src={CDN_URL + cloudinaryImageId} alt="restro-logo" />
+
+            <h3 className="font-bold py-4 text-lg">{name}</h3>
             <h4>{cuisines.join(', ')}</h4>
             <h4>{avgRating + ' stars'}</h4>
             <h4>{costForTwo}</h4>
