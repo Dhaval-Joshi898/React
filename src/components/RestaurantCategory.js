bb5b5beabb84022f04435ebcbf573cca2593e266
Dhaval <joshidhaval2002@gmail.com>
2024-11-28 16:43:03 +0530

    added accordian category name like recommended ,veg,desserts appetizers

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,17 +1,30 @@
-const RestaurantCategory=({data})=>{
-    // console.log(items)  //on the go destructuring it  category items data like recommended ,veg ,sides  data
-    return(
-        // <div className="my-6 font-bold content-between">
+import ItemList from "./ItemList";
+import { useState } from "react";
+const RestaurantCategory = ({ data }) => {
+    // console.log('category data',data)  //on the go destructuring it  category items data like recommended ,veg ,sides  data
+    const [itemsList,setItemsList]=useState(false)
+
+    return (
+       
         <div>
-            {/* Accordian Header */}
-            {/* ResCategory */}
-            <div className=" w-6/12 bg-gray-100 shadow-lg  my-6  mx-auto flex justify-between p-4">
-                <span>{data.title}</span>
-                <span>🔽</span>
+            <div onClick={()=>{
+                    if(itemsList===false){
+                    setItemsList(true)}
+                    else{
+                        setItemsList(false)
+                    }
+                    
+                }} className=" w-6/12 bg-gray-100 shadow-lg  my-6  mx-auto  p-4">
+                <div  className="flex justify-between cursor-pointer">
+                    <span className="font-bold">{data.title} ({data.itemCards.length})</span>
+                    <span>🔽</span>
+                </div>
+
+                {itemsList && <ItemList items={data.itemCards}></ItemList>}
+
             </div>
-        
-            {/* Accordian Body */}
         </div>
+
     )
 }
 
