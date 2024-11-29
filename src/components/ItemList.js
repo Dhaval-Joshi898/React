43b32cc407e24d824c31dc46a77a153a8c439353
Dhaval <joshidhaval2002@gmail.com>
2024-11-29 17:18:06 +0530

    lifting the states up Accordian feature

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -9,10 +9,10 @@ const ItemList = ({items}) => {
                     className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between" >
                     <div className="w-9/12">
                         <div className="py-2">
-                            <span>{item.card.info.name}</span>
-                            <span>
-                                - ₹
-                                {item.card.info.price
+                            <span className="font-bold">{item.card.info.name}{" "}</span>
+                            <span className="font-bold">
+                                - ₹{' '}
+                                 {item.card.info.price
                                     ? item.card.info.price / 100
                                     : item.card.info.defaultPrice / 100}
                             </span>
