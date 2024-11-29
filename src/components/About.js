ee1a57a24677c4befb38337aea021e510b4c6129
Dhaval <joshidhaval2002@gmail.com>
2024-11-29 20:34:37 +0530

    added UserContext name

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,36 @@
-import User from "./User";
-import UserClass from "./UserClassComponent";
 import React from "react";
+// import User from "./User";
+import UserContext from "../utils/UserContext";
+// import UserClass from "./UserClassComponent";
 
 
 class About extends React.Component{
     constructor(props){
         super(props)
-       console.log("Parent Class Construtor called")
+    //    console.log("Parent Class Construtor called")
     }
 
-    componentDidMount(){
-        console.log("Parent Component did mount")
-    }
+    // componentDidMount(){
+    //     console.log("Parent Component did mount")
+    // }
 
     render(){
     return( 
         <div className="about-section">
              <h1>About us Page</h1>
              <h3>About us page thorugh router</h3>
+             <UserContext.Consumer>
+                {({loggedInName})=><h1 className="font-bold">{loggedInName}</h1>}
+             </UserContext.Consumer>
              {console.log('Parent Class render called ')}
+
+
 
 
              {/* Class Based components -->how to pass props */}
              {/* <UserClass  name={'First Class'} location={"Bhayander"}/> */}
 
-             <User  name={'Dhaval Joshi'}/>
+             {/* <User  name={'Dhaval Joshi'}/> */}
             
 
              
