04916747516bad7b186276f8aa02849303244605
Dhaval <joshidhaval2002@gmail.com>
2024-11-29 20:33:02 +0530

    added useState to show name and setUsername from input text box

--- src/App.js
+++ src/App.js
@@ -1,62 +1,80 @@
-import React,{lazy, Suspense} from 'react';
-import ReactDOM from 'react-dom/client';
-import Header from './components/Header';    // you can write components/Header.js also 
-import Body from './components/Body';
-import About from './components/About';
-import Contact from './components/Contact';
-import Error from './components/Error';
-import RestaurantMenu from './components/RestaurantMenu.js';
-import Cart from './components/Cart';
-import { createBrowserRouter, RouterProvider,Outlet } from 'react-router-dom';
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header"; // you can write components/Header.js also
+import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu.js";
+import Cart from "./components/Cart";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import UserContext from "./utils/UserContext.js";
 
 //lazy takes a callback function with import method inside it has path to that Grocery component
-const Grocery=lazy(()=>import('./components/Grocery.js'))
+const Grocery = lazy(() => import("./components/Grocery.js"));
 
 const AppLayout = () => {
-    return (
-        <div className='app-container'>
-            <Header />
-            {/* <Body /> */}
-            <Outlet/>
-        </div>
-    )
-}
+  const [userName, setUserName] = useState();
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+  //authentication code
+  useEffect(() => {
+    const data = {
+      name: "Dhaval Joshi",
+    };
+
+    setUserName(data.name);
+  },[]);
+
+  return (
+    <UserContext.Provider value={{ loggedInName: userName ,setUserName}}>
+      <div className="app-container">
+        <Header />
+        {/* <Body /> */}
+        <Outlet />
+      </div>
+    </UserContext.Provider>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const appRouter = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
         path: "/",
-        element: <AppLayout />,
-        children: [{
-            path: "/",
-            element: <Body />,
-        },
-        {
-            path: "/about",
-            element: <About />,
-        },
-        {
-            path: "/contact",
-            element: <Contact />,
-        },
-        {
-            path:"/cart",
-            element:<Cart/>
-        },
-        {
-            path:"/grocery",
-            element:<Suspense fallback={<h1>Loading.....</h1>}> <Grocery/> </Suspense>
-        },
-        {
-            path:'/restaurant/:resId',
-            element:<RestaurantMenu/>
-        }],
-        errorElement: <Error />
-    },
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading.....</h1>}>
+            {" "}
+            <Grocery />{" "}
+          </Suspense>
+        ),
+      },
+      {
+        path: "/restaurant/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 
-]
-)
-
-
-root.render(<RouterProvider router={appRouter} />)
+root.render(<RouterProvider router={appRouter} />);
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,94 +1,125 @@
 import RestaurantCard from "./RestaurantCard";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import UserContext from "../utils/UserContext";
 
 const Body = () => {
+  // Super local State varaibles
+  // let [listOfRestaurants, setlist0fRestaurants] = useState(resList) //hooks
+  let [listOfRestaurants, setlist0fRestaurants] = useState([]); //hooks
+  const [filteredRestaurants, setfilteredRestaurants] = useState([]); //this state variable created to keep above variable intact(not touched or changed) and mnipulated
+  const [searchText, setSearchText] = useState("");
 
-    // Super local State varaibles
-    // let [listOfRestaurants, setlist0fRestaurants] = useState(resList) //hooks
-    let [listOfRestaurants, setlist0fRestaurants] = useState([]) //hooks
-    const [filteredRestaurants, setfilteredRestaurants] = useState([])  //this state variable created to keep above variable intact(not touched or changed) and mnipulated
-    const [searchText, setSearchText] = useState('')
+  const { loggedInName,setUserName } = useContext(UserContext);
+  console.log(loggedInName)
 
-    useEffect(() => {
-        console.log('after ui rendered body use effect');
-        fetchData();
-    }, [])
+  useEffect(() => {
+    console.log("after ui rendered body use effect");
+    fetchData();
+  }, []);
 
+  //fetching data from swiggy api
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2993420046926&lng=72.86991979871428&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
-    //fetching data from swiggy api
-    const fetchData = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2993420046926&lng=72.86991979871428&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const json = await data.json();
 
-        const json = await data.json()
+    setlist0fRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    ); //array jaa rha hai list0fRestaurants mein
+    setfilteredRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    ); //Will be using this manipulation like search filtered
+    console.log(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
+  // //THIS is called CONDITIONAL RENDERING        //on laoding give shimmer UI (skeleton UI)
+  //  if (listOfRestaurants.length === 0){
+  //     return <Shimmer />
+  //  }
 
-        setlist0fRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)  //array jaa rha hai list0fRestaurants mein
-        setfilteredRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)  //Will be using this manipulation like search filtered
-        console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  const onlineStatus = useOnlineStatus();
+  if (onlineStatus === false) {
+    return (
+      <div>
+        <h1>Looks Like you are offline!! Check Your Internet connection</h1>
+      </div>
+    );
+  }
 
-    }
+  //THE Below code is like if else using TERNARY OPERATOR
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter flex">
+        <div className="search m-4 p-4">
+          <input
+            type="text"
+            className="border border-solid border-black"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
 
+          <button
+            className="px-4 py-2 bg-green-100 m-4 rounded-lg"
+            onClick={() => {
+              let searchFiltered = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setfilteredRestaurants(searchFiltered);
+            }}
+          >
+            Search
+          </button>
+        </div>
 
-    // //THIS is called CONDITIONAL RENDERING        //on laoding give shimmer UI (skeleton UI)
-    //  if (listOfRestaurants.length === 0){
-    //     return <Shimmer />   
-    //  }
+        <div className="search m-4 p-4 flex items-center">
+          <button
+            className="px-4 py-2 bg-gray-100 rounded-lg"
+            onClick={() => {
+              let filtered = listOfRestaurants.filter(
+                (res) => res.info.avgRating > 4.1
+              );
+              console.log(filtered);
+              setfilteredRestaurants(filtered);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
 
-    const onlineStatus = useOnlineStatus();
-    if (onlineStatus === false) {
-        return (
-            <div>
-                <h1>Looks Like you are offline!! Check Your Internet connection</h1>
-            </div>
-        )
-    }
+        <div className="search m-4 p-4 flex items-center">
+          <input className="border border-black" 
+          value={loggedInName}
+          onChange={(e)=>setUserName(e.target.value)}></input>
+        </div>
+      </div>
 
-
-    //THE Below code is like if else using TERNARY OPERATOR
-    return listOfRestaurants.length === 0 ?
-        <Shimmer /> : (
-            <div className="body">
-                <div className='filter flex'>
-
-                    <div className="search m-4 p-4">
-                        <input type="text" className="border border-solid border-black" value={searchText} onChange={(e) => {
-                            setSearchText(e.target.value)
-                        }} ></input>
-
-                        <button className="px-4 py-2 bg-green-100 m-4 rounded-lg"
-                            onClick={() => {
-                                let searchFiltered = listOfRestaurants.filter(
-                                    (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()))
-                                setfilteredRestaurants(searchFiltered)
-                            }}>Search</button>
-                    </div>
-
-                    <div className="search m-4 p-4 flex items-center">
-                        <button className="px-4 py-2 bg-gray-100 rounded-lg"
-                            onClick={() => {
-                                let filtered = listOfRestaurants.filter((res) => res.info.avgRating > 4.1);
-                                console.log(filtered);
-                                setfilteredRestaurants(filtered)
-                            }}>Filter</button>
-                    </div>
-                </div>
-
-                <div className='flex flex-wrap'>
-
-                    {filteredRestaurants.map((restaurant) => {
-                        return <Link key={restaurant.info.id} 
-                        to={"/restaurant/" + restaurant.info.id} > 
-                        < RestaurantCard resData={restaurant} /> </Link>
-                        // return < RestaurantCard key={restaurant.data.id} resData={restaurant} />
-                        //key ,it is IMPORTANT to have unique key
-                    })}
-
-                </div>
-            </div>
-        )
-}
+      <div className="flex flex-wrap">
+        {filteredRestaurants.map((restaurant) => {
+          return (
+            <Link
+              key={restaurant.info.id}
+              to={"/restaurant/" + restaurant.info.id}
+            >
+              <RestaurantCard resData={restaurant} />{" "}
+            </Link>
+          );
+          // return < RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          //key ,it is IMPORTANT to have unique key
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
