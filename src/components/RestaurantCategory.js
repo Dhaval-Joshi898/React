3f98f978611f2a4956e76926807c7d2795585be5
Dhaval <joshidhaval2002@gmail.com>
2024-11-29 17:18:21 +0530

    lifting the state up accordian feature

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,31 +1,33 @@
 import ItemList from "./ItemList";
-import { useState } from "react";
-const RestaurantCategory = ({ data }) => {
-    // console.log('category data',data)  //on the go destructuring it  category items data like recommended ,veg ,sides  data
-    const [itemsList,setItemsList]=useState(false)
+const RestaurantCategory = ({ data, showItemList, setShowIndex, index }) => {
+  // console.log('category data',data)  //on the go destructuring it  category items data like recommended ,veg ,sides  data
 
-    return (
-       
-        <div>
-            <div onClick={()=>{
-                    if(itemsList===false){
-                    setItemsList(true)}
-                    else{
-                        setItemsList(false)
-                    }
-                    
-                }} className=" w-6/12 bg-gray-100 shadow-lg  my-6  mx-auto  p-4">
-                <div  className="flex justify-between cursor-pointer">
-                    <span className="font-bold">{data.title} ({data.itemCards.length})</span>
-                    <span>🔽</span>
-                </div>
+  const handleClick = () => {
+    setShowIndex(index);
+    // console.log(1)
+  };
 
-                {itemsList && <ItemList items={data.itemCards}></ItemList>}
-
-            </div>
+  return (
+    <div>
+      <div
+        onClick={handleClick}
+        className=" w-6/12 bg-gray-100 shadow-lg  my-6  mx-auto  p-4"
+      >
+        <div className="flex justify-between cursor-pointer">
+          <span className="font-bold">
+            {data.title} ({data.itemCards.length})
+          </span>
+          <span>🔽</span>
         </div>
 
-    )
-}
+        {showItemList && <ItemList items={data.itemCards}></ItemList>}
+     
+
+
+    </div>
+    </div>
+    
+  );
+};
 
 export default RestaurantCategory;
